d0e993993809a2c30f6da2fb78af059a7a48896d
Pedro Quijada Leyton <pedro.quijada@lacarretera.cl>
2015-07-19 20:37:10 +0100

    Form connected and working.

--- src/app/create/create.controller.js
+++ src/app/create/create.controller.js
@@ -7,6 +7,19 @@
 
 	/** @ngInject */
 	function CreateController($scope, $http, $stateParams){
-	
+
+		$scope.caca = function(){
+			//$scope.formData = {};
+			$http({
+					method: 'POST',
+					url: 'http://localhost:3030/api/tools/post',
+					data: $scope.formData
+            }).success(function(data) {
+               alert("inserted");
+            })
+            .error(function(data) {
+                alert("fail");
+            });
+		};
 	}
 })();
