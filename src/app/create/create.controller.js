4faee93e613bc9385e5bf176916362301d3380d9
James-Oldfield <hi@james-oldfield.co>
2015-07-21 21:57:37 +0100

    front end validation w/ http prefix etc

--- src/app/create/create.controller.js
+++ src/app/create/create.controller.js
@@ -6,10 +6,24 @@
 	.controller('CreateCtrl', CreateController);
 
 	/** @ngInject */
-	function CreateController($scope, $http, $stateParams, $state, createTool){
+	function CreateController($scope, $http, $stateParams, $state, createTool) {
 
-    $scope.submitTool = function(){
-      console.log($scope.formData);
+    // Add the 'http://' if not already present
+    $scope.prependHttp = function(url) {
+      if(url.indexOf('http') < 0) {
+        var prefixedUrl = 'http://' + url;
+        return prefixedUrl;
+      } else {
+        return url;
+      }
+    };
+
+    $scope.submitTool = function() {
+      // Sort out links
+      $scope.formData.git      = $scope.prependHttp($scope.formData.git);
+      $scope.formData.home     = $scope.prependHttp($scope.formData.home);
+      $scope.formData.download = $scope.prependHttp($scope.formData.download);
+
       createTool.post($scope.formData);
 		};
 
