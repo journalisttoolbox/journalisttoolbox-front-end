209dde78f6099653ccfe2d56cfcfd0487e3d662a
James-Oldfield <hi@james-oldfield.co>
2015-07-21 19:05:42 +0100

    form validation and conditional messages

--- src/app/create/create.controller.js
+++ src/app/create/create.controller.js
@@ -8,9 +8,11 @@
 	/** @ngInject */
 	function CreateController($scope, $http, $stateParams, $state, createTool){
 
-	    $scope.caca = function(){
-	      //$scope.formData = {};
-	      createTool.post($scope.formData);
+    $scope.submitTool = function(){
+      console.log($scope.formData);
+      createTool.post($scope.formData);
 		};
+
+    $scope.test = false;
 	}
 })();
