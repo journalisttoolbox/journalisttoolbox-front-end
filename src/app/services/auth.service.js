450a3786bcf9a6b86b9bc80d2defb2b49226d4a3
Nevo Segal <nevosegal@gmail.com>
2015-07-22 15:28:31 +0100

    basic signup form works

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -1,81 +1,84 @@
-// 'use strict';
+'use strict';
 
-// angular.module('journalisttoolboxFrontend')
-//   .factory('Auth', function Auth($location, $rootScope, $cookies) {
-//     $rootScope.currentUser = $cookieStore.get('user') || null;
-//     $cookieStore.remove('user');
+angular.module('journalisttoolboxFrontend')
+  .factory('Auth', function Auth($http, $location, $rootScope, $cookies) {
+    // $rootScope.currentUser = $cookieStore.get('user') || null;
+    // $cookieStore.remove('user');
 
-//     return {
+    return {
 
-//       login: function(provider, user, callback) {
-//         var cb = callback || angular.noop;
-//         Session.save({
-//           provider: provider,
-//           email: user.email,
-//           password: user.password,
-//           rememberMe: user.rememberMe
-//         }, function(user) {
-//           $rootScope.currentUser = user;
-//           return cb();
-//         }, function(err) {
-//           return cb(err.data);
-//         });
-//       },
+      // login: function(provider, user, callback) {
+      //   var cb = callback || angular.noop;
+      //   Session.save({
+      //     provider: provider,
+      //     email: user.email,
+      //     password: user.password,
+      //     rememberMe: user.rememberMe
+      //   }, function(user) {
+      //     $rootScope.currentUser = user;
+      //     return cb();
+      //   }, function(err) {
+      //     return cb(err.data);
+      //   });
+      // },
 
-//       logout: function(callback) {
-//         var cb = callback || angular.noop;
-//         Session.delete(function(res) {
-//             $rootScope.currentUser = null;
-//             return cb();
-//           },
-//           function(err) {
-//             return cb(err.data);
-//           });
-//       },
+      // logout: function(callback) {
+      //   var cb = callback || angular.noop;
+      //   Session.delete(function(res) {
+      //       $rootScope.currentUser = null;
+      //       return cb();
+      //     },
+      //     function(err) {
+      //       return cb(err.data);
+      //     });
+      // },
 
-//       createUser: function(userinfo, callback) {
-//         var cb = callback || angular.noop;
-//         User.save(userinfo,
-//           function(user) {
-//             $rootScope.currentUser = user;
-//             return cb();
-//           },
-//           function(err) {
-//             return cb(err.data);
-//           });
-//       },
+      createUser: function(userInfo, callback) {
+        var cb = callback || angular.noop;
+        $http({
+          method:'POST', 
+          url:'http://localhost:3030/api/users', 
+          data: userInfo
+        }).success(function(data){
+            console.log("success");
+            cb();
+          })
+          .error(function(err){
+            console.log("There was an error: " + err);
+          });
+      }
 
-//       currentUser: function() {
-//         Session.get(function(user) {
-//           $rootScope.currentUser = user;
-//         });
-//       },
+      // currentUser: function() {
+      //   Session.get(function(user) {
+      //     $rootScope.currentUser = user;
+      //   });
+      // },
 
-//       changePassword: function(email, oldPassword, newPassword, callback) {
-//         var cb = callback || angular.noop;
-//         User.update({
-//           email: email,
-//           oldPassword: oldPassword,
-//           newPassword: newPassword
-//         }, function(user) {
-//             console.log('password changed');
-//             return cb();
-//         }, function(err) {
-//             return cb(err.data);
-//         });
-//       },
+      // changePassword: function(email, oldPassword, newPassword, callback) {
+      //   var cb = callback || angular.noop;
+      //   User.update({
+      //     email: email,
+      //     oldPassword: oldPassword,
+      //     newPassword: newPassword
+      //   }, function(user) {
+      //       console.log('password changed');
+      //       return cb();
+      //   }, function(err) {
+      //       return cb(err.data);
+      //   });
+      // },
 
-//       removeUser: function(email, password, callback) {
-//         var cb = callback || angular.noop;
-//         User.delete({
-//           email: email,
-//           password: password
-//         }, function(user) {
-//             console.log(user + 'removed');
-//             return cb();
-//         }, function(err) {
-//             return cb(err.data);
-//         });
-//       }
-//     };
-//   })
+      // removeUser: function(email, password, callback) {
+      //   var cb = callback || angular.noop;
+      //   User.delete({
+      //     email: email,
+      //     password: password
+      //   }, function(user) {
+      //       console.log(user + 'removed');
+      //       return cb();
+      //   }, function(err) {
+      //       return cb(err.data);
+      //   });
+      // }
+    };
+  })
--- src/app/signup/signup.controller.js
+++ src/app/signup/signup.controller.js
@@ -1,12 +1,26 @@
 (function(){
 	'use strict';
+
 	angular
 		.module('journalisttoolboxFrontend')
-		.controller('SignupCtrl', ['$scope', function($scope){
+		.controller('SignupCtrl', SignupController);
+
+		/** @ngInject */
+		function SignupController($scope, $http, Auth, $state){
 
 			$scope.register = function(){
-
+				Auth.createUser({
+					firstName: $scope.fname,
+					lastName: $scope.lname,
+					email: $scope.email,
+					username: $scope.username,
+					password: $scope.pass
+				},
+				function(){
+					$state.go('home');
+				});
 			};
-		}]);
+
+		}
 
 })();
