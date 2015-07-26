23273ae06516029365feff85002863c181f94e00
Pedro Quijada Leyton <pedro.quijada@lacarretera.cl>
2015-07-26 13:54:44 +0100

    Created form and scripts to allow filter by platforms and more

--- src/app/components/search/search.directive.js
+++ src/app/components/search/search.directive.js
@@ -18,8 +18,15 @@
 			$scope.term = "";
 			$scope.filters = {};
 			$scope.tools = [];
+			$scope.ShowFilter = false; 
 			var allTools = {};
 			var pterm = '';
+			//filter var			
+			$scope.FilterTools = [];
+			$scope.Findit = false;
+
+	        //starting the semantic UI accordion
+	        $('.ui.accordion').accordion();
 
 			toolService.toolResource.query()
         .$promise.then(function(data) {
@@ -29,11 +36,44 @@
 			$scope.onKeyUp = function(){
 				if($scope.term.length >= 3){
 					$scope.tools = allTools;
+					//show the filter options
+					$scope.ShowFilter = true;
 				}
 				else{
-					$scope.tools = [];
+					$scope.tools = [];		
+					//hide the filter options			
+					$scope.ShowFilter = false; 
 				}
 			};
+
+			$scope.includeFilter = function(name) {
+	            var i = $.inArray(name, $scope.FilterTools);
+	            if (i > -1) {
+	                $scope.FilterTools.splice(i, 1);
+	            } else {
+	                $scope.FilterTools.push(name);
+	            }
+        	}
+
+        	$scope.toolFilter = function(tool) {
+        	$scope.Findit  = false;
+
+            if ($scope.FilterTools.length > 0) {
+                //array platorms
+            	for (var i = tool.platform.length - 1; i >= 0; i--) {
+            		if ($.inArray(tool.platform[i], $scope.FilterTools) >= 0)
+                        $scope.Findit  = true;
+            	};          
+                //free or not
+                if ($.inArray(tool.price, $scope.FilterTools) >= 0)
+                    $scope.Findit  = true; 
+            }      
+            //if no filter or tool find, return the tool
+            if($scope.Findit == true || $scope.FilterTools.length == 0)
+    			return tool;
+            else
+            	return;
+        	}
 		}
 	}
 
