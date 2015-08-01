6b1378c2f066d7982ac4e004c4b986214114cc0b
James-Oldfield <hi@james-oldfield.co>
2015-08-01 19:03:54 +0100

    adding tool routes auth

--- server/api/tool/index.js
+++ server/api/tool/index.js
@@ -2,16 +2,18 @@
 
 var express = require('express');
 var controller = require('./tool.controller');
+var config = require('../../config/environment');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
 router.get('/category/:name', controller.category);
 router.get('/', controller.index);
-router.get('/user/:userID', controller.getUserTools);
+router.get('/user/:userID', auth.isAuthenticated(), controller.getUserTools);
 router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.put);
-router.patch('/:id', controller.put);
-router.delete('/:id', controller.destroy);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.put);
+router.patch('/:id', auth.isAuthenticated(), controller.put);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
