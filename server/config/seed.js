14b5553d2f2e20d6bd5dfd9a7178586e8b00378d
James-Oldfield <hi@james-oldfield.co>
2015-08-01 11:23:38 +0100

    disabling dummy populate

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,20 +7,20 @@
 
 var User = require('../api/user/user.model');
 
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
-    }
-  );
-});
+// User.find({}).remove(function() {
+//   User.create({
+//     provider: 'local',
+//     name: 'Test User',
+//     email: 'test@test.com',
+//     password: 'test'
+//   }, {
+//     provider: 'local',
+//     role: 'admin',
+//     name: 'Admin',
+//     email: 'admin@admin.com',
+//     password: 'admin'
+//   }, function() {
+//       console.log('finished populating users');
+//     }
+//   );
+// });
