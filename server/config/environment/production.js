ae87d67e3d6efb02b7c33c47e2e5bf519443a9b7
Nevo Segal <nevosegal@gmail.com>
2016-05-04 10:22:49 +0100

    changed path to db

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/jt'
+            'mongodb://newsroom:newsr00m@ds011432.mlab.com:11432/heroku_80dbht3c'
   }
 };
