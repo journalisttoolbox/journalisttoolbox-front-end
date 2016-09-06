f476e27da177956640233833bac0045042b05e97
Nevo Segal <nevosegal@gmail.com>
2016-09-06 10:49:50 +0100

    added footers, fixed responsiveness of about page as part of #88

--- client/components/errSrc/errSrc.directive.js
+++ client/components/errSrc/errSrc.directive.js
@@ -11,7 +11,7 @@
       link: function(scope, element, attrs) {
         element.bind('error', function() {
           if (attrs.src != attrs.errSrc) {
-            attrs.$set('src', attrs.errSrc);
+            attrs.$set('ngSrc', attrs.errSrc);
           }
         });
       }
